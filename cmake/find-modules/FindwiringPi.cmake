9ad20a5ebac149d49f2e8897e0c60ae0e5584aa3
Bartek Łukawski <bwmn.peter@gmail.com>
2019-09-20 22:01:04 +0200

    Fetch wiringPi from asrob-uc3m clone, adapt find module script

--- cmake/find-modules/FindwiringPi.cmake
+++ cmake/find-modules/FindwiringPi.cmake
@@ -15,11 +15,15 @@
 # URL: https://github.com/asrob-uc3m/yarp-devices
 
 if(NOT wiringPi_INCLUDE_DIR)
-    find_path(wiringPi_INCLUDE_DIR wiringPi.h)
+    find_path(wiringPi_INCLUDE_DIR NAMES wiringPi.h
+                                   HINTS $ENV{wiringPi_ROOT}
+                                   PATH_SUFFIXES include)
 endif()
 
 if(NOT wiringPi_LIBRARY)
-    find_library(wiringPi_LIBRARY wiringPi)
+    find_library(wiringPi_LIBRARY NAMES wiringPi
+                                  HINTS $ENV{wiringPi_ROOT}
+                                  PATH_SUFFIXES lib)
 endif()
 
 if(NOT wiringPi_LIBRARY_DEV)
@@ -28,17 +32,20 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(wiringPi FOUND_VAR wiringPi_FOUND
-                                           REQUIRED_VARS wiringPi_INCLUDE_DIR wiringPi_LIBRARY wiringPi_LIBRARY_DEV)
+                                           REQUIRED_VARS wiringPi_INCLUDE_DIR wiringPi_LIBRARY)
 
 if(wiringPi_FOUND)
     add_library(wiringPi::wiringPi UNKNOWN IMPORTED)
-    add_library(wiringPi::wiringPiDev UNKNOWN IMPORTED)
 
     set_target_properties(wiringPi::wiringPi PROPERTIES IMPORTED_LOCATION "${wiringPi_LIBRARY}"
                                                         INTERFACE_INCLUDE_DIRECTORIES "${wiringPi_INCLUDE_DIR}")
 
-    set_target_properties(wiringPi::wiringPiDev PROPERTIES IMPORTED_LOCATION "${wiringPi_LIBRARY_DEV}"
-                                                           INTERFACE_INCLUDE_DIRECTORIES "${wiringPi_INCLUDE_DIR}")
+    if(wiringPi_LIBRARY_DEV)
+        add_library(wiringPi::wiringPiDev UNKNOWN IMPORTED)
+
+        set_target_properties(wiringPi::wiringPiDev PROPERTIES IMPORTED_LOCATION "${wiringPi_LIBRARY_DEV}"
+                                                               INTERFACE_INCLUDE_DIRECTORIES "${wiringPi_INCLUDE_DIR}")
+    endif()
 endif()
 
 mark_as_advanced(wiringPi_INCLUDE_DIR wiringPi_LIBRARY wiringPi_LIBRARY_DEV)
